df8fa87835f0f7af9fa90a5d29cbed5d73947168
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-09 17:39:24 +0500

    ResestTransform hotkey added to CustomhotkeySetter script

--- Assets/Editor/CustomHotkeysSetter.cs
+++ Assets/Editor/CustomHotkeysSetter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using UnityEngine;
+using UnityEditor;
 
 public class CustomHotkeysSetter
 {
@@ -9,5 +10,18 @@ public class CustomHotkeysSetter
             EditorApplication.ExecuteMenuItem("File/Save");
 
         EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+    [MenuItem("GameObject/CustomHotkeys/ResetTransform #R")]
+    static void ResetTransform()
+    {
+        var selectedObject = Selection.activeGameObject;
+
+        if(selectedObject != null)
+        {
+            selectedObject.transform.position = Vector3.zero;
+            selectedObject.transform.rotation = Quaternion.identity;
+            selectedObject.transform.localScale = Vector3.one;
+        }
     }
 }
