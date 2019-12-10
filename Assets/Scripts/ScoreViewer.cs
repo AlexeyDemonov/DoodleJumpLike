faf30c4526df08e1bf4fba8f04c712a17127444d
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-10 18:58:44 +0500

    ScoreViewer implemented

---
+++ Assets/Scripts/ScoreViewer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ScoreViewer : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreLabel;
+    public int ScoreIncrease;
+
+    int _score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _score = 0;
+        ScoreLabel.text = "000000";
+        LevelReporter.LevelLowered += IncreaseScore;
+    }
+
+    // This function is called when the MonoBehaviour will be destroyed
+    private void OnDestroy()
+    {
+        LevelReporter.LevelLowered -= IncreaseScore;
+    }
+
+    void IncreaseScore()
+    {
+        _score += ScoreIncrease;
+        ScoreLabel.text = _score.ToString("D6");
+    }
+}
