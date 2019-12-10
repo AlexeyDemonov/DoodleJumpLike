fcf4616ed6893ace4804326155ea9ccde9921a41
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-10 19:31:25 +0500

    DifficultyIncreaser implementation

---
+++ Assets/Scripts/DifficultyIncreaser.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(GameManager))]
+public class DifficultyIncreaser : MonoBehaviour
+{
+    public int IncreaseEveryXLevels;
+    public float IncreaseScrollSpeedBy;
+
+    int _levelCounter;
+    GameManager _gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelCounter = 0;
+        _gameManager = GetComponent<GameManager>();
+
+        LevelReporter.LevelLowered += IncreaseDifficulty;
+    }
+
+    // This function is called when the MonoBehaviour will be destroyed
+    private void OnDestroy()
+    {
+        LevelReporter.LevelLowered -= IncreaseDifficulty;
+    }
+
+    void IncreaseDifficulty()
+    {
+        _levelCounter++;
+
+        if(_levelCounter >= IncreaseEveryXLevels)
+        {
+            _levelCounter = 0;
+            _gameManager.ScrollSpeed += IncreaseScrollSpeedBy;
+        }
+    }
+}
