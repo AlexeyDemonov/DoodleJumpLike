68f924c770cd02861afc49065dcc4de84271dce1
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-10 17:51:33 +0500

    GameManager implementation Changed: GameManager now has full functionality, it scrolls, queues and dequeues levels as well as increases its speed on player vertical movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,31 +7,62 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] Levels;
-    public Vector2 InstantiatePosition;
+    public float InstantiateOffset;
     public float ScrollSpeed;
     [Tooltip("To increase scroll speed based on player Y position")]
     public Transform Player;
 
     bool _rotateLevel;
-    Queue<GameObject> _levelQueue;
+    LinkedList<GameObject> _levelQueue;
+    float _currentScrollSpeed;
+    Vector3 _instantiateOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         _rotateLevel = UnityEngine.Random.Range(0,2) == 0 ? true : false;
-        _levelQueue = new Queue<GameObject>();
-        InstantiateNextLevel(new Vector2(0f,0f));
-        InstantiateNextLevel(InstantiatePosition);
+        _levelQueue = new LinkedList<GameObject>();
+
+        //Create first three levels manually
+        _instantiateOffset = Vector3.zero;
+        InstantiateNextLevel();
+        _instantiateOffset = new Vector3(0f, InstantiateOffset, 0f);
+        InstantiateNextLevel();
+        InstantiateNextLevel();
 
         GetComponent<GameStarter>().GameStarted += () => StartCoroutine(ScrollLevels());
+        GetComponent<GameStarter>().GameStarted += () => StartCoroutine(ModifyScrollSpeedIfPlayerIsHigh());
+
+        LevelReporter.LevelLowered += HandleLevelLowered;
     }
 
-    void InstantiateNextLevel(Vector2 position)
+    // This function is called when the MonoBehaviour will be destroyed
+    private void OnDestroy()
+    {
+        LevelReporter.LevelLowered -= HandleLevelLowered;
+    }
+
+    void HandleLevelLowered()
+    {
+        DeleteLowestLevel();
+        InstantiateNextLevel();
+    }
+
+    void DeleteLowestLevel()
+    {
+        var toDestroy = _levelQueue.First.Value;
+        _levelQueue.RemoveFirst();
+        Destroy(toDestroy);
+    }
+
+    void InstantiateNextLevel()
     {
         var level = Levels[ UnityEngine.Random.Range(0, Levels.Length) ];
+        var position = (_levelQueue.Last?.Value.transform.position ?? Vector3.zero) + _instantiateOffset;
         var rotation = _rotateLevel ? Quaternion.Euler(0f, 180f, 0f) : Quaternion.identity;
         var levelClone = (GameObject)Instantiate(level, position, rotation);
-        _levelQueue.Enqueue(levelClone);
+
+        _levelQueue.AddLast(levelClone);
         _rotateLevel = !_rotateLevel;
     }
 
@@ -41,7 +72,25 @@ public class GameManager : MonoBehaviour
         {
             foreach (var item in _levelQueue)
             {
-                item.transform.Translate(Vector3.down * ScrollSpeed * Time.deltaTime);
+                if(item != null)//If it is not in the process of destroying
+                    item.transform.Translate(Vector3.down * _currentScrollSpeed * Time.deltaTime);
+            }
+
+            yield return null;//Just wait for next Update
+        }
+    }
+
+    IEnumerator ModifyScrollSpeedIfPlayerIsHigh()
+    {
+        while (true)
+        {
+            if(Player.position.y > 1)
+            {
+                _currentScrollSpeed = ScrollSpeed * Player.position.y;
+            }
+            else if(_currentScrollSpeed != ScrollSpeed)
+            {
+                _currentScrollSpeed = ScrollSpeed;
             }
 
             yield return null;//Just wait for next Update
