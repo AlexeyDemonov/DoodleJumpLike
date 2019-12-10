6aa75698c55d9c969220ad1b0a9e7ce5c838d338
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-10 12:58:44 +0500

    Organized scripts into folders Also added: GameStarter script - raising 'GameStarted' event on first player jump

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour
+{
+    public event Action GameStarted;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            GameStarted?.Invoke();
+            this.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformScripts/LevelReporter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(FallChecker))]
+public class LevelReporter : MonoBehaviour
+{
+    public static event Action LevelLowered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<FallChecker>().Fallen += () => LevelLowered.Invoke();
+    }
+}
